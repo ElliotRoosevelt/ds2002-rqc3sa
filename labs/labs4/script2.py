0d81bd4662d3cd56ae5d371121b369ad04a315b7
ElleP <143535896+ElliotRoosevelt@users.noreply.github.com>
2025-04-30 17:22:17 -0400

    Update script2.py

--- labs/labs4/script2.py
+++ labs/labs4/script2.py
@@ -4,14 +4,13 @@ import boto3
 s3=boto3.client(‘s3’)
 
 # vars needed
-bucket_name = str
-object_name = str
-expires_in = int
+bucket_name = ‘bucket’
+object_name = ‘object’
+expires_in = 3000
 
 #fetch
 def download_file(url, object_name):
 urllib.request.urlretrieve(url, local_filename)
-print(response)
 
 #upload
 def upload_file(file_name. bucket_name. object_name):
@@ -22,8 +21,18 @@ upload_file(“file.png”. “ds2002-rqc3sa”. “newname.png”)
 
 
 #output presigned
-response = s3.generate_presigned_url(
-    'get_object',
-    Params={'Bucket': bucket_name, 'Key': object_name},
-    ExpiresIn=expires_in
-)
+def generate_presigned_url(bucket_name, object_name, expires_in):
+    response = s3.generate_presigned_url(
+        'get_object',
+        Params={'Bucket':bucket, 'Key':object},
+        ExpiresIn=expires_in
+    )
+    return response
+
+url = ‘https://img.freepik.com/free-psd/macaroon-isolated-transparent-background_191095-35017.jpg?semt=ais_hybrid&w=740'  
+local_filename = 'newname.png'
+download_file(url, local_filename)
+upload_file(local_filename, 'bucket', 'newname.png')
+presigned_url = generate_presigned_url('bucket', 'newname.png', expires_in)
+
+
